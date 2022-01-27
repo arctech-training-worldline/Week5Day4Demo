2943727e82807cd6669d20e73b14f56a9c8409e9
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-01-27 11:17:57 +0530

    refactoring code to improve design

--- Week5Day4Demo/FootBall.cs
+++ Week5Day4Demo/FootBall.cs
@@ -13,10 +13,12 @@ namespace Week5Day4Demo
     internal class FootBall
     {
         private readonly Form _form;
+        private readonly PictureBox _pictureBox;
 
         public FootBall(Form form)
         {
             _form = form;
+            _pictureBox = CreatePictureBoxWithFootBallImage();
         }
 
         public void Bounce()
@@ -27,12 +29,10 @@ namespace Week5Day4Demo
 
         private void WorkerThreadBounceBall()
         {
-            var pictureBox = CreatePictureBoxWithFootBallImage();
-
             var random = new Random();
 
-            var x = random.Next(0, _form.Width - pictureBox.Width);
-            var y = random.Next(0, _form.Height - pictureBox.Height);
+            var x = random.Next(0, _form.Width - _pictureBox.Width);
+            var y = random.Next(0, _form.Height - _pictureBox.Height);
             var position = new Point(x, y);
 
             var xFactor = random.Next(5, 20);
@@ -47,8 +47,8 @@ namespace Week5Day4Demo
                 // out of the window before bouncing back.
                 // If we bounce the left and top of the ball on the imaginary line
                 // the ball always stays visible inside the form
-                var rightImaginaryLine = _form.Width - pictureBox.Width;
-                var bottomImaginaryLine = _form.Height - pictureBox.Height;
+                var rightImaginaryLine = _form.Width - _pictureBox.Width;
+                var bottomImaginaryLine = _form.Height - _pictureBox.Height;
 
                 if (position.X < 0 || position.X > rightImaginaryLine)
                     xFactor = -xFactor;
@@ -56,10 +56,10 @@ namespace Week5Day4Demo
                 if (position.Y < 0 || position.Y > bottomImaginaryLine)
                     yFactor = -yFactor;
 
-                pictureBox.Invoke((MethodInvoker)delegate
+                _pictureBox.Invoke((MethodInvoker)delegate
                 {
-                    pictureBox.Left = position.X;
-                    pictureBox.Top = position.Y;
+                    _pictureBox.Left = position.X;
+                    _pictureBox.Top = position.Y;
                 });
 
                 Thread.Sleep(10);
@@ -69,19 +69,15 @@ namespace Week5Day4Demo
 
         private PictureBox CreatePictureBoxWithFootBallImage()
         {
-            var pictureBoxFootBall = new PictureBox();
-            pictureBoxFootBall.Left = 100;
-            pictureBoxFootBall.Top = 100;
-            pictureBoxFootBall.ImageLocation = @"C:\Users\avina\OneDrive - arctechinfo.com\Documents\Training\Sessions\C#\27-Jan-2022\Week5Day4\Week5Day4Demo\football.jpg";
-            pictureBoxFootBall.SizeMode = PictureBoxSizeMode.AutoSize;
-
-            // Cannot do this in Thread. Have to use invoke
-            //_form.Controls.Add(pictureBoxFootBall);
-
-            _form.Invoke((MethodInvoker)delegate
+            var pictureBoxFootBall = new PictureBox
             {
-                _form.Controls.Add(pictureBoxFootBall);
-            });
+                Left = 100,
+                Top = 100,
+                ImageLocation = @"C:\Users\avina\OneDrive - arctechinfo.com\Documents\Training\Sessions\C#\27-Jan-2022\Week5Day4\Week5Day4Demo\football.jpg",
+                SizeMode = PictureBoxSizeMode.AutoSize
+            };
+
+            _form.Controls.Add(pictureBoxFootBall);
 
             return pictureBoxFootBall;
         }
